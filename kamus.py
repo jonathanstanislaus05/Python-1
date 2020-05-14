4968a8bff2fdd3a6275e4c1d52663b3a7bfe38ec
jonathanstanislaus05 <jonathanstanislaus05@gmail.com>
2020-05-14 13:58:21 +0700

    Added feature

---
+++ kamus.py
@@ -0,0 +1,4 @@
+ages = {"Samuel": 15, "Bobi": 17 }
+ages ["Mario"] = 14
+ages ["Samuel"] += 1
+print(ages)
--- name.py
+++ name.py
@@ -1,2 +1,6 @@
 name = input()
-print (f"hello, {name}!")
+print(f"Hello, {name}!")
+
+name = "Jonathan"
+coordinate = [10.0, 20.0]
+names = ["Jonathan", "Samuel", "Mario"]
